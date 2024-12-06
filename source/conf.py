1a6459b39628502522e50518020dc0c30288a42e
sablem <lemlsabah@gmail.com>
2024-12-06 15:04:49 +0100

    second commit

--- source/conf.py
+++ source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
 project = 'Lunettes intelligentes'
 copyright = '2024, aya'
 author = 'aya'
@@ -14,7 +18,12 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
